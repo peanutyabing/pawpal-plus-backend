fd2c100d070922ca5f1450bfc01a914ae933a4d9
peanutyabing <sherry.liuyabing@gmail.com>
2023-04-15 15:32:24 +0800

    Seed more pets and events data

--- db/seeders/20230411093352-pets.js
+++ db/seeders/20230411093352-pets.js
@@ -12,7 +12,7 @@ module.exports = {
           breed_id: 2,
           name: "Ellen",
           image_url:
-            "https://www.petfinder.com/static/039c6cd5ebd9f7819dd6eaa6998f4b4d/27d28/PF_Calico_600x260.webp",
+            "https://images.unsplash.com/photo-1592328555746-1047e76a71bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80",
           date_of_birth: new Date("2020-04-09"),
           created_at: new Date(),
           updated_at: new Date(),
@@ -23,8 +23,19 @@ module.exports = {
           breed_id: 4,
           name: "Carl",
           image_url:
-            "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2023/02/09141023/Diana-the-Poodle.jpg",
+            "https://images.unsplash.com/photo-1581562324420-eff2f5aaa4b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
           date_of_birth: new Date("2022-11-02"),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          user_id: 1,
+          species_id: 1,
+          breed_id: 2,
+          name: "Manja",
+          image_url:
+            "https://images.unsplash.com/photo-1560948799-e17458123a9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+          date_of_birth: new Date("2018-04-01"),
           created_at: new Date(),
           updated_at: new Date(),
         },
