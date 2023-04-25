6de9eafa105731a72dab1387b3dea64e698d8887
peanutyabing <sherry.liuyabing@gmail.com>
2023-04-25 21:23:32 +0800

    Adjust pets seeder

--- db/seeders/20230411093352-pets.js
+++ db/seeders/20230411093352-pets.js
@@ -9,7 +9,7 @@ module.exports = {
         {
           user_id: 1,
           species_id: 1,
-          breed_id: 2,
+          breed_id: 28,
           name: "Ellen",
           image_url:
             "https://images.unsplash.com/photo-1592328555746-1047e76a71bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80",
@@ -31,7 +31,7 @@ module.exports = {
         {
           user_id: 1,
           species_id: 1,
-          breed_id: 2,
+          breed_id: 28,
           name: "Manja",
           image_url:
             "https://images.unsplash.com/photo-1560948799-e17458123a9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
